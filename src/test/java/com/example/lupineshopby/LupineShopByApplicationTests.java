268145944006104d48bc3d2183f14c68bef27b12
Dowoon <woodw920@gmail.com>
2023-11-11 16:59:23 +0900

    Initial commit

---
+++ src/test/java/com/example/lupineshopby/LupineShopByApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.lupineshopby;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LupineShopByApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
