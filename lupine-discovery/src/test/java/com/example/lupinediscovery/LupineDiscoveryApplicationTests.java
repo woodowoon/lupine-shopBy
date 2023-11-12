4231dc36aeec56367c7135162b028ade7fcf20e7
Dowoon <woodw920@gmail.com>
2023-11-12 23:08:14 +0900

    spring-cloud-starter-loadbalancer 설정 진행 중

---
+++ lupine-discovery/src/test/java/com/example/lupinediscovery/LupineDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lupinediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LupineDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
