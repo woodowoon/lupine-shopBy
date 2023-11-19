34ac7a0171c550c53d0ce1db50170aaa9daac7d1
ddlee <vktm91@nate.com>
2023-11-19 21:39:34 +0900

    [Discovery/Display] eureka 세팅

--- lupine-display/src/main/java/kr/co/lupinedisplay/LupineDisplayApplication.java
+++ lupine-display/src/main/java/kr/co/lupinedisplay/LupineDisplayApplication.java
@@ -2,9 +2,7 @@ package kr.co.lupinedisplay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "kr.co.lupinedisplay")
 @SpringBootApplication
 public class LupineDisplayApplication {
     public static void main(String[] args) {
